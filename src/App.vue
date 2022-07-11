f0021357a51e2dabde6284fd4a4b232ac1e24e55
Klincher <camelotvm48@gmail.com>
2022-07-11 14:05:46 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="d-flex justify-content-center">
+      <div class="col-2">
+        <form>
+          <select class="form-control" v-model="selectedType">
+            <option v-for="ty in types" v-bind:value="ty.name" v-bind:key="ty">
+              {{ ty.name }}
+            </option>
+          </select>
+          <span>Выбрано: {{ selectedMark }}</span>
+          <br />
+
+          <select class="form-control" v-model="selectedMark">
+            <option
+              v-for="mark in marks[selectedType]"
+              :value="mark.name"
+              v-bind:key="mark"
+            >
+              {{ mark.name }}
+            </option>
+          </select>
+          <span>Выбрано: {{ selectedMark }}</span>
+          <br />
+
+          <select class="form-control" v-model="selectedModel">
+            <option
+              v-for="model in models[selectedMark]"
+              v-bind:value="model.name"
+              v-bind:key="model"
+            >
+              {{ model.name }}
+            </option>
+          </select>
+          <span>Выбрано: {{ selectedModel }}</span>
+          <br />
+          <button class="btn btn-primary" type="submit">Ok</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      types: [{ name: "light" }, { name: "heavy" }],
+      selectedType: "",
+
+      marks: {
+        light: [{ name: "reno" }],
+        heavy: [{ name: "kraz" }],
+      },
+      selectedMark: "",
+
+      models: {
+        reno: [{ name: "clio" }],
+        kraz: [{ name: "vepr" }],
+      },
+      selectedModel: "",
+    };
+  },
+};
 </script>
 
 <style>
